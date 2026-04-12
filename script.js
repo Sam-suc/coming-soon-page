d5720b9db586bf2e4c35b25397e3f97a5fef9317
unknown <sami.work2130>
2026-04-12 16:03:41 -0700

    Create the form validation with JS and style it

---
+++ script.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("email-form");
+const emailInput = document.getElementById("email-input");
+const errorIcon = document.getElementById("error-icon");
+const errorMessage = document.getElementById("error-message");
+
+form.addEventListener("submit", (e) => {
+  const emailValue = emailInput.value;
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //standard email regex
+
+  console.log(emailValue);
+
+  if (!emailPattern.test(emailValue)) {
+    // prevent the form from being send
+    e.preventDefault();
+
+    // display the error icon and error text
+    errorIcon.classList.remove("hidden");
+    errorMessage.classList.remove("hidden");
+
+    // add red border
+    emailInput.style.borderColor = "hsl(0, 82%, 59%)";
+  } else {
+    // If valid hide errors
+    errorIcon.classList.add("hidden");
+    errorMessage.classList.add("hidden");
+    emailInput.style.borderColor = "";
+  }
+});
